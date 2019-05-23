7c366320dfc2906262abfe289f4b1e3ac413e476
tomcphr <tomcphr@pm.me>
2019-05-23 09:16:26 +0100

    Initial commit

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,15 @@
+export default class GameScene extends Phaser.Scene
+{
+    constructor() {
+        super();
+    };
+
+    preload() {
+    };
+
+    create() {
+    };
+
+    update() {
+    };
+}
